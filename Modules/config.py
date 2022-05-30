f999202c40e1cedf51cbb3a2280a9199d051fbc3
Guldfaxe <azis.prasetya93@gmail.com>
2022-05-31 00:14:04 +0700

    for generate token

--- Modules/config.py
+++ Modules/config.py
@@ -6,36 +6,36 @@ b2b_new_cust = {
 }
 
 model_elasticity_retail = {
-    "ST" : "model_3_nasional_st.pkl",
-    "SG" : "model_3_nasional_sg.pkl",
-    "DYNAMIX":"model_3_nasional_dynamix.pkl",
-    "POWERMAX": "model_3_nasional_powermax.pkl",
-    "SP":"model_3_nasional_sp.pkl",
-    "ANDALAS":"model_3_nasional_andalas.pkl",
-    "MASONRY":"model_3_nasional_masonry.pkl"
+	"ST" : "model_3_nasional_st.pkl",
+	"SG" : "model_3_nasional_sg.pkl",
+	"DYNAMIX":"model_3_nasional_dynamix.pkl",
+	"POWERMAX": "model_3_nasional_powermax.pkl",
+	"SP":"model_3_nasional_sp.pkl",
+	"ANDALAS":"model_3_nasional_andalas.pkl",
+	"MASONRY":"model_3_nasional_masonry.pkl"
 }
 
 model_pricing_retail = {
-    "ST" : "model_5_nasional_st_price (1).pkl",
-    "SG" : "model_5_nasional_sg_price (1).pkl",
-    "DYNAMIX":"model_5_nasional_dynamix_price (1).pkl",
-    "POWERMAX": "model_5_nasional_powermax_price (1).pkl",
-    "SP":"model_5_nasional_sp_price (1).pkl",
-    "ANDALAS":"model_5_nasional_andalas_price (1).pkl",
-    "MASONRY":"model_5_nasional_masonry_price (1).pkl"
+	"ST" : "model_5_nasional_st_price (1).pkl",
+	"SG" : "model_5_nasional_sg_price (1).pkl",
+	"DYNAMIX":"model_5_nasional_dynamix_price (1).pkl",
+	"POWERMAX": "model_5_nasional_powermax_price (1).pkl",
+	"SP":"model_5_nasional_sp_price (1).pkl",
+	"ANDALAS":"model_5_nasional_andalas_price (1).pkl",
+	"MASONRY":"model_5_nasional_masonry_price (1).pkl"
 }
 
 
 model_elasticity_b2b = {
-    "DURAMAX" : "elasticity_brand_sig_duramax_model_b2b_random_forest.pkl",
-    "OPC Premium" : "elasticity_brand_sig_opc_premium_model_b2b_random_forest.pkl",
-    "OPC Reguler":"elasticity_brand_sig_opc_reguler_model_b2b_random_forest.pkl",
-    "OWC":"elasticity_brand_sig_owc_model_b2b_random_forest.pkl",
-    "PCC":"elasticity_brand_sig_pcc_model_b2b_random_forest.pkl",
-    "PCC Premium":"elasticity_brand_sig_pcc_premium_model_b2b_random_forest.pkl",
-    "SBC":"elasticity_brand_sig_sbc_model_b2b_random_forest.pkl",
-    "Type II":"elasticity_brand_sig_type_ii_model_b2b_random_forest.pkl",
-    "Type V":"elasticity_brand_sig_type_v_model_b2b_random_forest.pkl"
+	"DURAMAX" : "elasticity_brand_sig_duramax_model_b2b_random_forest.pkl",
+	"OPC Premium" : "elasticity_brand_sig_opc_premium_model_b2b_random_forest.pkl",
+	"OPC Reguler":"elasticity_brand_sig_opc_reguler_model_b2b_random_forest.pkl",
+	"OWC":"elasticity_brand_sig_owc_model_b2b_random_forest.pkl",
+	"PCC":"elasticity_brand_sig_pcc_model_b2b_random_forest.pkl",
+	"PCC Premium":"elasticity_brand_sig_pcc_premium_model_b2b_random_forest.pkl",
+	"SBC":"elasticity_brand_sig_sbc_model_b2b_random_forest.pkl",
+	"Type II":"elasticity_brand_sig_type_ii_model_b2b_random_forest.pkl",
+	"Type V":"elasticity_brand_sig_type_v_model_b2b_random_forest.pkl"
 }
 
 path_data_mapping_bisnis = './Modules/data/mapping_kategori_logika_bisnis.csv'
